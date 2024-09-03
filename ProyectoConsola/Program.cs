b047fd8e8da93ae4a3016ff6b99cf3b2b04c8384
VivianaQuihuis <viviana_quihuis24@unikino@edu.mx>
2024-09-03 08:02:45 -0700

    creando mi primer variable

--- ProyectoConsola/Program.cs
+++ ProyectoConsola/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hola Ci");
+
+            string miPrimerVariable;
+            miPrimerVariable = "Hola2";
+            Console.WriteLine(miPrimerVariable);
         }
     }
 }
